3d26137abae336055b40d2b5c6ed85787bb6ec73
KimLeoVal <kimleo@mail.ru>
2022-06-21 18:58:51 +0600

    'end commit'

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -7,6 +7,9 @@ from webapp import checker
 n = 4
 secret_nums = checker.Check()
 secret_nums = secret_nums.generate_numbers(n)
+num_list = []
+step_list = []
+step = {'step': 0}
 
 
 def index_view(request):
@@ -16,22 +19,21 @@ def index_view(request):
 
     if request.method == 'POST':
         int_num = []
-        num_list = []
-        step = 0
+
         try:
             num = request.POST.get('user_num')
             num = num.split(" ")
             num = list(num)
             num_list.append(num)
-            step += 1
+            step['step'] += 1
+            step_list.append(step['step'])
             try:
                 for i in num:
                     i = int(i)
                     int_num.append(i)
             except ValueError:
                 context = {'error': "Enter 4 different numbers from 1 to 9",
-                           'num_list': num_list,
-                           'step': step}
+                           }
                 return render(request, 'index.html', context)
             try:
                 check_num = checker.Check()
@@ -39,32 +41,21 @@ def index_view(request):
                     game = checker.Check()
                     res = game.checker_game(int_num, secret_nums)
                     context = {'res': res,
-                               'num_list': num_list,
-                               'step': step
 
-                       }
+                               }
             except ValueError:
                 context = {'error': "Enter 4 different numbers from 1 to 9",
-                           'num_list': num_list,
-                           'step': step}
+                           }
                 return render(request, 'index.html', context)
         except ValueError:
             context = {'error': "You didn't enter anything",
-                       'num_list': num_list,
-                       'step': step}
+                       }
             return render(request, 'index.html', context)
         return render(request, 'index.html', context)
 
 
-
 def history_view(request):
-    step = 0
-    while True:
-        a = index_view(request)
-        step += 1
-        print(a)
-
-        break
-        num = a
-    context = {'user_num': num}
+    context = {'step': step_list,
+               'num_list': num_list
+               }
     return render(request, 'history.html', context)
